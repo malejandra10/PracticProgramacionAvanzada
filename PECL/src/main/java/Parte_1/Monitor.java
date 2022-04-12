5df0f249bdf41bf72017c43aeefb23412d2e6f82
Blanca Calderón <79267061+BlancaCalderon@users.noreply.github.com>
2022-04-12 19:18:46 +0200

    Update Monitor.java
    Atributos del monitor y metodos basicos

--- PECL/src/main/java/Parte_1/Monitor.java
+++ PECL/src/main/java/Parte_1/Monitor.java
@@ -9,6 +9,37 @@ package Parte_1;
  *
  * @author blanf
  */
-public class Monitor {
+public class Monitor extends Thread{
+    private String id;
+    private Campamento camp;
+    
+    /*Constructor*/
+    public Monitor(String i, Campamento c)
+    {
+        this.id = i;
+        this.camp = c;
+    }
+
+    //Método devuelve identificador del monitor
+    public String getMId() {
+        return id;
+    }
+
+    //Método establece identificador del monitor
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    //Ejecuta hilo
+    public void run()
+    {
+        try
+        {
+            sleep((int)(3000*Math.random()));
+        } catch (InterruptedException e){ }
+        camp.entrar(this); //Entra en el campamento si hay hueco; y sino espera en la cola
+        //camp.accederActividad(this); //Entra  actividad correspondiente
+    }
     
 }
+
