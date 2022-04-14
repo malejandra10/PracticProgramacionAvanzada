d8238f28a85380ae669899f4712dd6acfb2cf5cc
Blanca Calderón <79267061+BlancaCalderon@users.noreply.github.com>
2022-04-14 13:51:18 +0200

    Update Child.java
    clase niño on atributos identificador y contador del numero de actividades que han realizado (atomic)

--- PECL/src/main/java/Parte_1/Child.java
+++ PECL/src/main/java/Parte_1/Child.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*Niños que entraran al campamento
  */
 package Parte_1;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * @author blanf
  */
-public class Child {
+public class Child extends Thread{
+    private String id;
+    private Campamento camp;
+    AtomicInteger contActividades = new AtomicInteger(0);
+
+
+    public Child(String id, Campamento c, AtomicInteger cont) {
+        this.id = id;
+        camp = c;
+        contActividades = cont;
+    }
+
+    public String getCId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public  AtomicInteger getContActividades() {
+        return contActividades;
+    }
+
+    public void setContActividades( AtomicInteger contActividades) {
+        this.contActividades = contActividades;
+    }
     
+    public void run()
+    {
+        try
+        {
+            sleep((int)(3000*Math.random()));
+        } catch (InterruptedException e){ }
+        camp.entrar(this); //Entra en el campamento si hay hueco; y sino espera en la cola
+    }
 }
